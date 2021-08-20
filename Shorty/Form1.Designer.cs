85212bcf0f3a3e8a3e369c5f85faba91597d5f8d
msyef <maabdelhamid.seif@gmail.com>
2021-08-21 00:56:55 +0300

    update1

--- Shorty/Form1.Designer.cs
+++ Shorty/Form1.Designer.cs
@@ -31,9 +31,12 @@ namespace Shorty
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Shorty));
             this.mainbar = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.minmizeBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.inputTxt = new System.Windows.Forms.TextBox();
             this.logo = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.multiPnl = new System.Windows.Forms.Panel();
             this.mainbar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +44,10 @@ namespace Shorty
             // 
             this.mainbar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainbar.BackgroundImage")));
             this.mainbar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.mainbar.Controls.Add(this.textBox1);
+            this.mainbar.Controls.Add(this.closeBtn);
+            this.mainbar.Controls.Add(this.minmizeBtn);
+            this.mainbar.Controls.Add(this.addBtn);
+            this.mainbar.Controls.Add(this.inputTxt);
             this.mainbar.Controls.Add(this.logo);
             this.mainbar.Dock = System.Windows.Forms.DockStyle.Top;
             this.mainbar.Location = new System.Drawing.Point(0, 0);
@@ -49,13 +55,70 @@ namespace Shorty
             this.mainbar.Size = new System.Drawing.Size(450, 55);
             this.mainbar.TabIndex = 0;
             // 
-            // panel1
+            // closeBtn
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 55);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(450, 218);
-            this.panel1.TabIndex = 1;
+            this.closeBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("closeBtn.BackgroundImage")));
+            this.closeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.ImageKey = "(none)";
+            this.closeBtn.Location = new System.Drawing.Point(411, 16);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(27, 23);
+            this.closeBtn.TabIndex = 1;
+            this.closeBtn.UseVisualStyleBackColor = false;
+            // 
+            // minmizeBtn
+            // 
+            this.minmizeBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.minmizeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.minmizeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("minmizeBtn.BackgroundImage")));
+            this.minmizeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.minmizeBtn.FlatAppearance.BorderSize = 0;
+            this.minmizeBtn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.minmizeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.minmizeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.minmizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minmizeBtn.ImageKey = "(none)";
+            this.minmizeBtn.Location = new System.Drawing.Point(378, 16);
+            this.minmizeBtn.Name = "minmizeBtn";
+            this.minmizeBtn.Size = new System.Drawing.Size(27, 23);
+            this.minmizeBtn.TabIndex = 1;
+            this.minmizeBtn.UseVisualStyleBackColor = false;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.addBtn.BackColor = System.Drawing.Color.Transparent;
+            this.addBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("addBtn.BackgroundImage")));
+            this.addBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.addBtn.FlatAppearance.BorderSize = 0;
+            this.addBtn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.addBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.addBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addBtn.ImageKey = "(none)";
+            this.addBtn.Location = new System.Drawing.Point(345, 16);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(27, 23);
+            this.addBtn.TabIndex = 1;
+            this.addBtn.UseVisualStyleBackColor = false;
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputTxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputTxt.Location = new System.Drawing.Point(75, 17);
+            this.inputTxt.MaxLength = 25;
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.PlaceholderText = "What are you looking for ? :)";
+            this.inputTxt.Size = new System.Drawing.Size(252, 22);
+            this.inputTxt.TabIndex = 0;
             // 
             // logo
             // 
@@ -68,14 +131,13 @@ namespace Shorty
             this.logo.Size = new System.Drawing.Size(60, 55);
             this.logo.TabIndex = 0;
             // 
-            // textBox1
+            // multiPnl
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(79, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "What are you looking for ? :)";
-            this.textBox1.Size = new System.Drawing.Size(254, 16);
-            this.textBox1.TabIndex = 0;
+            this.multiPnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.multiPnl.Location = new System.Drawing.Point(0, 55);
+            this.multiPnl.Name = "multiPnl";
+            this.multiPnl.Size = new System.Drawing.Size(450, 218);
+            this.multiPnl.TabIndex = 1;
             // 
             // Shorty
             // 
@@ -83,7 +145,7 @@ namespace Shorty
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(450, 273);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.multiPnl);
             this.Controls.Add(this.mainbar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Shorty";
@@ -99,9 +161,12 @@ namespace Shorty
         #endregion
 
         private System.Windows.Forms.Panel mainbar;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox inputTxt;
         private System.Windows.Forms.Panel logo;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel multiPnl;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button minmizeBtn;
+        private System.Windows.Forms.Button addBtn;
     }
 }
 
---
+++ Shorty/appitem.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Shorty
+{
+    partial class appitem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(appitem));
+            this.panelitem = new System.Windows.Forms.Panel();
+            this.remBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.itemLabel = new System.Windows.Forms.Label();
+            this.itemIcon = new System.Windows.Forms.PictureBox();
+            this.panelitem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.itemIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelitem
+            // 
+            this.panelitem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelitem.BackgroundImage")));
+            this.panelitem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelitem.Controls.Add(this.remBtn);
+            this.panelitem.Controls.Add(this.editBtn);
+            this.panelitem.Controls.Add(this.itemLabel);
+            this.panelitem.Controls.Add(this.itemIcon);
+            this.panelitem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelitem.Location = new System.Drawing.Point(0, 0);
+            this.panelitem.Name = "panelitem";
+            this.panelitem.Size = new System.Drawing.Size(450, 55);
+            this.panelitem.TabIndex = 0;
+            // 
+            // remBtn
+            // 
+            this.remBtn.BackColor = System.Drawing.Color.Transparent;
+            this.remBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("remBtn.BackgroundImage")));
+            this.remBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.remBtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.remBtn.FlatAppearance.BorderSize = 0;
+            this.remBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.remBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.remBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.remBtn.Location = new System.Drawing.Point(402, 16);
+            this.remBtn.Name = "remBtn";
+            this.remBtn.Size = new System.Drawing.Size(26, 23);
+            this.remBtn.TabIndex = 2;
+            this.remBtn.UseVisualStyleBackColor = false;
+            // 
+            // editBtn
+            // 
+            this.editBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("editBtn.BackgroundImage")));
+            this.editBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.editBtn.FlatAppearance.BorderSize = 0;
+            this.editBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.editBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.editBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editBtn.Location = new System.Drawing.Point(360, 16);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(26, 23);
+            this.editBtn.TabIndex = 2;
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // itemLabel
+            // 
+            this.itemLabel.AutoSize = true;
+            this.itemLabel.Location = new System.Drawing.Point(66, 20);
+            this.itemLabel.Name = "itemLabel";
+            this.itemLabel.Size = new System.Drawing.Size(0, 15);
+            this.itemLabel.TabIndex = 1;
+            // 
+            // itemIcon
+            // 
+            this.itemIcon.Dock = System.Windows.Forms.DockStyle.Left;
+            this.itemIcon.Location = new System.Drawing.Point(0, 0);
+            this.itemIcon.Name = "itemIcon";
+            this.itemIcon.Size = new System.Drawing.Size(60, 55);
+            this.itemIcon.TabIndex = 0;
+            this.itemIcon.TabStop = false;
+            // 
+            // appitem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panelitem);
+            this.Name = "appitem";
+            this.Size = new System.Drawing.Size(450, 55);
+            this.panelitem.ResumeLayout(false);
+            this.panelitem.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.itemIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelitem;
+        private System.Windows.Forms.Button remBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Label itemLabel;
+        private System.Windows.Forms.PictureBox itemIcon;
+    }
+}
